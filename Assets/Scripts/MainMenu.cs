2f23ae7d373d5d66ab397a2c91dc66136e72631d
Aram Plaza Bel <arampb84@gmail.com>
2023-11-29 22:03:52 +0100

    Menu Animations

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private List<Sprite> sprites;
+    [SerializeField] private Image character;
+
+    private void Start()
+    {
+        int randomPos = Random.Range(0, sprites.Count - 1);
+        character.sprite = sprites[randomPos];
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
